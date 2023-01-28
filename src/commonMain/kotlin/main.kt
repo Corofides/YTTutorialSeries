2444842530bf04b4689f2453a81d55df836914ce
James Lee Lendrem <J.l.lendrem@smoothlines.net>
2023-01-28 17:57:55 +0000

    Resolution information.

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -9,11 +9,26 @@ import com.soywiz.korim.format.*
 import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.*
 import com.soywiz.korma.interpolation.*
+import kotlin.math.*
 
-suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
+suspend fun main() = Korge(width = 512, height = 512, virtualWidth = 256, virtualHeight = 256, bgcolor = Colors["#2b2b2b"]) {
 	val sceneContainer = sceneContainer()
 
-	sceneContainer.changeTo({ CollisionScene() })
+	sceneContainer.changeTo({ ResolutionAndCoordinateScene() })
+}
+
+class ResolutionAndCoordinateScene : Scene() {
+
+    override suspend fun SContainer.sceneMain() {
+
+        val mouseNativeCoords = text("0,0").centerOn(this)
+
+        mouseNativeCoords.addUpdater {
+            text = "${round(input.mouse.x)},${round(input.mouse.y)}"
+        }
+
+    }
+
 }
 
 class CollisionScene : Scene() {
