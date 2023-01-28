e687cf14bb9fa2130fe3b77b89e703c59fc990df
James Lee Lendrem <J.l.lendrem@smoothlines.net>
2023-01-28 14:43:04 +0000

    Check if old sample code works as there's a difference between this and a prior version. It does so you don't need the scene but I will continue with it in for now.

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -10,9 +10,26 @@ import com.soywiz.korma.geom.*
 import com.soywiz.korma.interpolation.*
 
 suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
-	val sceneContainer = sceneContainer()
+	// val sceneContainer = sceneContainer()
 
-	sceneContainer.changeTo({ MyScene() })
+    //Test if old sample code still works?
+    val minDegrees = (-16).degrees
+    val maxDegrees = (+16).degrees
+
+    val image = image(resourcesVfs["korge.png"].readBitmap()) {
+        rotation = maxDegrees
+        anchor(.5, .5)
+        scale(0.8)
+        position(256, 256)
+    }
+
+    while (true) {
+        image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
+        image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
+    }
+
+
+	//sceneContainer.changeTo({ MyScene() })
 }
 
 class MyScene : Scene() {
